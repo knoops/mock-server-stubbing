233dea6ba75d4c8222403bb8a3d01d48f8ba0c6f
Bert Massop <Bert.Massop@ing.nl>
2017-05-16 14:55:44 +0200

    Configure static call response

--- src/main/java/nl/ing/api/MockServerApp.java
+++ src/main/java/nl/ing/api/MockServerApp.java
@@ -3,12 +3,58 @@ package nl.ing.api;
 import org.mockserver.client.server.MockServerClient;
 
 import static org.mockserver.integration.ClientAndServer.startClientAndServer;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+import static org.mockserver.model.JsonBody.json;
 
 public class MockServerApp {
 
-    private static MockServerClient mockServerClient = startClientAndServer(1080);
+    private static MockServerClient mockServerClient = startClientAndServer(9000);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        configure();
+        System.out.println("Started");
+        Thread.sleep(0);
+    }
 
+    static void configure() {
+        mockServerClient.when(
+                request()
+                        .withPath("/2010-04-01/Accounts/AC11111111111111111111111111111111/Calls.json")
+                        .withMethod("POST")
+        ).respond(
+                response()
+                        .withBody(json("{\n" +
+                                "  \"sid\": \"CA11111111111111111111111111111111\",\n" +
+                                "  \"date_created\": null,\n" +
+                                "  \"date_updated\": null,\n" +
+                                "  \"parent_call_sid\": null,\n" +
+                                "  \"account_sid\": \"AC11111111111111111111111111111111\",\n" +
+                                "  \"to\": \"+31611111111\",\n" +
+                                "  \"to_formatted\": \"+31611111111\",\n" +
+                                "  \"from\": \"+31200000000\",\n" +
+                                "  \"from_formatted\": \"+31200000000\",\n" +
+                                "  \"phone_number_sid\": \"PN11111111111111111111111111111111\",\n" +
+                                "  \"status\": \"queued\",\n" +
+                                "  \"start_time\": null,\n" +
+                                "  \"end_time\": null,\n" +
+                                "  \"duration\": null,\n" +
+                                "  \"price\": null,\n" +
+                                "  \"price_unit\": \"USD\",\n" +
+                                "  \"direction\": \"outbound-api\",\n" +
+                                "  \"answered_by\": null,\n" +
+                                "  \"api_version\": \"2010-04-01\",\n" +
+                                "  \"annotation\": null,\n" +
+                                "  \"forwarded_from\": null,\n" +
+                                "  \"group_sid\": null,\n" +
+                                "  \"caller_name\": null,\n" +
+                                "  \"uri\": \"/2010-04-01/Accounts/AC11111111111111111111111111111111/Calls/CA11111111111111111111111111111111.json\",\n" +
+                                "  \"subresource_uris\": {\n" +
+                                "    \"notifications\": \"/2010-04-01/Accounts/AC11111111111111111111111111111111/Calls/CA11111111111111111111111111111111/Notifications.json\",\n" +
+                                "    \"recordings\": \"/2010-04-01/Accounts/AC11111111111111111111111111111111/Calls/CA11111111111111111111111111111111/Recordings.json\"\n" +
+                                "  }\n" +
+                                "}")
+                        )
+        );
     }
 }
--- src/test/java/nl/ing/api/TestStaticEndpoint.java
+++ src/test/java/nl/ing/api/TestStaticEndpoint.java
@@ -1,27 +1,22 @@
 package nl.ing.api;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.web.client.RestTemplate;
 
 public class TestStaticEndpoint {
 
-    private final String staticUrl = "http://localhost:9000/twilio/callsstatic";
+    private final String staticUrl = "http://localhost:9000/2010-04-01/Accounts/AC11111111111111111111111111111111/Calls.json";
+
+    @Before
+    public void setUp() {
+        MockServerApp.configure();
+    }
 
     @Test
     public void testStaticEndpoint() {
-        Delivery delivery = new Delivery();
-        delivery.deliveryId = "1";
-        delivery.item = "item";
-        callBackForStatus(staticUrl, delivery, "OK");
-
-    }
-
-    static void callBackForStatus(String url, Delivery delivery, String status) {
-        Delivery callbackDelivery = new Delivery();
-        callbackDelivery.item = delivery.item;
-        callbackDelivery.deliveryId = delivery.deliveryId;
-        callbackDelivery.deliveryStatus = status;
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.postForObject(url, callbackDelivery, String.class);
+        String response = restTemplate.postForObject(staticUrl, "put-request-body-here", String.class);
+        System.err.println(response);
     }
 }
